c9024eab1f129072124bb48498edf67ace790b75
Rayhaan <r53ahmed@uwaterloo.ca>
2016-08-25 14:10:59 -0400

    Updates js

--- canvas2.js
+++ canvas2.js
@@ -1,97 +1,99 @@
 ///////CANVAS 2///////
-//Globals
-var canvas = document.getElementById('two');
-var context = canvas.getContext('2d');
-var width = canvas.width;
-var height = canvas.height;
-var particles = [];
-var numParticles = 100;
-var linear = true;
+var particleSystem = function () {
+	//Globals
+	var canvas = document.getElementById('two');
+	var context = canvas.getContext('2d');
+	var width = canvas.width;
+	var height = canvas.height;
+	var particles = [];
+	var numParticles = 100;
+	var linear = true;
 
-//Initialize with black background
-context.fillStyle = 'rgba(0, 0, 0, 1)';
-context.fillRect(0, 0, width, height);
+	//Initialize with black background
+	context.fillStyle = 'rgba(0, 0, 0, 1)';
+	context.fillRect(0, 0, width, height);
 
-//Constructor very similar to canvas1's constructor
-function Particle() {
-	this.x = Math.random() * width;
-	this.y = Math.random() * height;
-	this.vx = Math.random() * 8 - 4;
-	this.vy = Math.random() * 8 - 4;
-	this.radius = Math.random() * 20 + 10;
-	this.colour = "rgb(" + Math.floor(Math.random() * 255) + ","
-								+ Math.floor(Math.random() * 255) + ","
-								+ Math.floor(Math.random() * 255) + ")";
-}
-
-
-Particle.prototype.update = function() {
-	//If linear movement
-	if(linear) {
-		this.x += this.vx;
-		this.y += this.vy;
+	//Constructor very similar to canvas1's constructor
+	function Particle() {
+		this.x = Math.random() * width;
+		this.y = Math.random() * height;
+		this.vx = Math.random() * 8 - 4;
+		this.vy = Math.random() * 8 - 4;
+		this.radius = Math.random() * 20 + 10;
+		this.colour = "rgb(" + Math.floor(Math.random() * 255) + ","
+									+ Math.floor(Math.random() * 255) + ","
+									+ Math.floor(Math.random() * 255) + ")";
 	}
-	//If random movement
-	else if(!linear) {
-		this.x += Math.cos(this.vx) + 0.1;
-		this.y += Math.sin(this.vy) + 0.1;
-		if(this.vx < 2000000000)
-			this.vx += 0.05;
-			this.vy += 0.03;
-		} else {
-			this.vx = 0;
-			this.vy = 0;
+
+
+	Particle.prototype.update = function() {
+		//If linear movement
+		if(linear) {
+			this.x += this.vx;
+			this.y += this.vy;
 		}
-	//If moves off screen
-	if(this.x < -60) this.x = width + 60;
-	if(this.y < -60) this.y = height + 60;
-	if(this.x > width + 60) this.x = -60;
-	if(this.y > height + 60) this.y = -60;
-}
-
-function draw() {
-	context.globalCompositeOperation = "source-over"; //Make sure it overwrites screen
-	context.fillStyle = 'rgba(0, 0, 0, 0.3)'; //Allow for partial trailing
-	context.fillRect(0, 0, width, height); //Clear
-	context.globalCompositeOperation = "lighter"; //Lighten colours
-	for(var i = 0; i < numParticles; i++) {
-		particles[i].update(); //Update positions
-		context.beginPath();
-		context.arc(particles[i].x, particles[i].y, particles[i].radius, 0, Math.PI * 2, false);
-		var gradient = context.createRadialGradient(particles[i].x, particles[i].y, 0, particles[i].x,
-																	particles[i].y, particles[i].radius);
-		gradient.addColorStop(0, "white");
-		gradient.addColorStop(0.4, "white");
-		gradient.addColorStop(0.4, particles[i].colour);
-		gradient.addColorStop(1, "black");
-		context.fillStyle = gradient;
-		context.fill();
+		//If random movement
+		else if(!linear) {
+			this.x += Math.cos(this.vx) + 0.1;
+			this.y += Math.sin(this.vy) + 0.1;
+			if(this.vx < 2000000000)
+				this.vx += 0.05;
+				this.vy += 0.03;
+			} else {
+				this.vx = 0;
+				this.vy = 0;
+			}
+		//If moves off screen
+		if(this.x < -60) this.x = width + 60;
+		if(this.y < -60) this.y = height + 60;
+		if(this.x > width + 60) this.x = -60;
+		if(this.y > height + 60) this.y = -60;
 	}
-}
 
-//Create particle array
-for(var i = 0; i < numParticles; i++) {
-	particles.push(new Particle());
-}
-
-//Switch between linear and random movement
-canvas.addEventListener("click", function() {
-	if(linear) {
-		linear = false;
-	} else if (!linear) {
+	function draw() {
+		context.globalCompositeOperation = "source-over"; //Make sure it overwrites screen
+		context.fillStyle = 'rgba(0, 0, 0, 0.3)'; //Allow for partial trailing
+		context.fillRect(0, 0, width, height); //Clear
+		context.globalCompositeOperation = "lighter"; //Lighten colours
 		for(var i = 0; i < numParticles; i++) {
-			linear = true;
-			//New constant velocities
-			particles[i].vx = Math.random() * 12 - 4;
-			particles[i].vy = Math.random() * 12 - 4;
+			particles[i].update(); //Update positions
+			context.beginPath();
+			context.arc(particles[i].x, particles[i].y, particles[i].radius, 0, Math.PI * 2, false);
+			var gradient = context.createRadialGradient(particles[i].x, particles[i].y, 0, particles[i].x,
+																		particles[i].y, particles[i].radius);
+			gradient.addColorStop(0, "white");
+			gradient.addColorStop(0.4, "white");
+			gradient.addColorStop(0.4, particles[i].colour);
+			gradient.addColorStop(1, "black");
+			context.fillStyle = gradient;
+			context.fill();
 		}
 	}
-}, false);
 
-//Loop
-function loop() {
-	draw();
+	//Create particle array
+	for(var i = 0; i < numParticles; i++) {
+		particles.push(new Particle());
+	}
+
+	//Switch between linear and random movement
+	canvas.addEventListener("click", function() {
+		if(linear) {
+			linear = false;
+		} else if (!linear) {
+			for(var i = 0; i < numParticles; i++) {
+				linear = true;
+				//New constant velocities
+				particles[i].vx = Math.random() * 12 - 4;
+				particles[i].vy = Math.random() * 12 - 4;
+			}
+		}
+	}, false);
+
+	//Loop
+	function loop() {
+		draw();
+		requestAnimationFrame(loop);
+	}
+
 	requestAnimationFrame(loop);
-}
-
-requestAnimationFrame(loop);
+} ();
--- canvas3.js
+++ canvas3.js
@@ -1,92 +1,94 @@
 ///////CANVAS 3///////
-//Globals
-var canvas = document.getElementById('three');
-var context = canvas.getContext('2d');
-var width = canvas.width;
-var height = canvas.height;
-var particles = [];
-var numParticles = 30;
+var ribbons = function() {
+	//Globals
+	var canvas = document.getElementById('three');
+	var context = canvas.getContext('2d');
+	var width = canvas.width;
+	var height = canvas.height;
+	var particles = [];
+	var numParticles = 30;
 
-//Initialize with black background
-context.fillStyle = 'rgba(0, 0, 0, 1)';
-context.fillRect(0, 0, width, height);
+	//Initialize with black background
+	context.fillStyle = 'rgba(0, 0, 0, 1)';
+	context.fillRect(0, 0, width, height);
 
-function Particle() {
-	this.x = Math.random() * width;
-	this.y = Math.random() * height;
-	this.velocity = Math.random() * 2 + 1;
-	this.radius = 0;
-	this.rotAngle = Math.random() * 360;
-	this.colour = "rgb(" + Math.floor(Math.random() * 255) + ","
-								+ Math.floor(Math.random() * 255) + ","
-								+ Math.floor(Math.random() * 255) + ")";
-	this.lineWeight = 1;
-}
-
-Particle.prototype.update = function() {
-	this.x += this.velocity * Math.cos(this.rotAngle);
-	this.y += this.velocity * Math.sin(this.rotAngle);
-
-	if(this.x < 0) this.x = width;
-	if(this.x > width) this.x = 0;
-	if(this.y > height) this.y = 0;
-	if(this.y < 0) this.y = height;
-}
-
-function draw() {
-	context.globalCompositeOperation = "source-over"; //Make sure it overwrites screen
-	context.fillStyle = 'rgba(0, 0, 0, 0.05)'; //Allow for partial trailing
-	context.fillRect(0, 0, width, height); //Clear
-	context.globalCompositeOperation = "lighten"; //Lighten colours
-	for(var i = 0; i < numParticles; i++) {
-		context.fillStyle = "white";
-		context.fillRect(particles[i].x, particles[i].y, particles[i].radius, particles[i].radius);
-
-		for(var n = 0; n < numParticles; n++) {
-			var distance = Math.sqrt(Math.pow(particles[n].x - particles[i].x, 2) + Math.pow(particles[n].y - particles[i].y, 2));
-			if(distance < 150) {
-				context.beginPath();
-				context.lineWidth = particles[i].lineWeight;
-				context.moveTo(particles[i].x, particles[i].y);
-				context.lineTo(particles[n].x, particles[n].y);
-				context.strokeStyle = particles[i].colour;
-				context.stroke();
-			}
-		}
-		particles[i].update();
-	}
-}
-
-for(var i = 0; i < numParticles; i++) {
-	particles.push(new Particle());
-}
-
-canvas.addEventListener("click", function() {
-	for(var i = 0; i < numParticles; i++) {
-		particles[i].colour = "rgb(" + Math.floor(Math.random() * 255) + ","
+	function Particle() {
+		this.x = Math.random() * width;
+		this.y = Math.random() * height;
+		this.velocity = Math.random() * 2 + 1;
+		this.radius = 0;
+		this.rotAngle = Math.random() * 360;
+		this.colour = "rgb(" + Math.floor(Math.random() * 255) + ","
 									+ Math.floor(Math.random() * 255) + ","
 									+ Math.floor(Math.random() * 255) + ")";
+		this.lineWeight = 1;
 	}
-}, true);
 
-canvas.addEventListener("keydown", function(e) {
-	if(e.keyCode == 187) {
-		console.log(e.keyCode);
+	Particle.prototype.update = function() {
+		this.x += this.velocity * Math.cos(this.rotAngle);
+		this.y += this.velocity * Math.sin(this.rotAngle);
+
+		if(this.x < 0) this.x = width;
+		if(this.x > width) this.x = 0;
+		if(this.y > height) this.y = 0;
+		if(this.y < 0) this.y = height;
+	}
+
+	function draw() {
+		context.globalCompositeOperation = "source-over"; //Make sure it overwrites screen
+		context.fillStyle = 'rgba(0, 0, 0, 0.05)'; //Allow for partial trailing
+		context.fillRect(0, 0, width, height); //Clear
+		context.globalCompositeOperation = "lighten"; //Lighten colours
 		for(var i = 0; i < numParticles; i++) {
-			if(particles[i].lineWeight < 7)
-				particles[i].lineWeight++;
+			context.fillStyle = "white";
+			context.fillRect(particles[i].x, particles[i].y, particles[i].radius, particles[i].radius);
+
+			for(var n = 0; n < numParticles; n++) {
+				var distance = Math.sqrt(Math.pow(particles[n].x - particles[i].x, 2) + Math.pow(particles[n].y - particles[i].y, 2));
+				if(distance < 150) {
+					context.beginPath();
+					context.lineWidth = particles[i].lineWeight;
+					context.moveTo(particles[i].x, particles[i].y);
+					context.lineTo(particles[n].x, particles[n].y);
+					context.strokeStyle = particles[i].colour;
+					context.stroke();
+				}
+			}
+			particles[i].update();
 		}
 	}
-	if(e.keyCode == 189) {
-		for(var i = 0; i < numParticles; i++) {
-			if(particles[i].lineWeight > 1)
-				particles[i].lineWeight--;
-		}
-	}
-}, true);
 
-function loop() {
-	draw();
+	for(var i = 0; i < numParticles; i++) {
+		particles.push(new Particle());
+	}
+
+	canvas.addEventListener("click", function() {
+		for(var i = 0; i < numParticles; i++) {
+			particles[i].colour = "rgb(" + Math.floor(Math.random() * 255) + ","
+										+ Math.floor(Math.random() * 255) + ","
+										+ Math.floor(Math.random() * 255) + ")";
+		}
+	}, true);
+
+	canvas.addEventListener("keydown", function(e) {
+		if(e.keyCode == 187) {
+			console.log(e.keyCode);
+			for(var i = 0; i < numParticles; i++) {
+				if(particles[i].lineWeight < 7)
+					particles[i].lineWeight++;
+			}
+		}
+		if(e.keyCode == 189) {
+			for(var i = 0; i < numParticles; i++) {
+				if(particles[i].lineWeight > 1)
+					particles[i].lineWeight--;
+			}
+		}
+	}, true);
+
+	function loop() {
+		draw();
+		requestAnimationFrame(loop);
+	}
 	requestAnimationFrame(loop);
-}
-requestAnimationFrame(loop);
+} ();
